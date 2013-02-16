7608b79e726940f07b41af6a7d19a31e918c507c
Kasper Johansen <k@spernj.org>
2013-02-16 16:11:49 +0100

    Various work on tests for products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  validates :title, :description, :image_url, presence: true, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, uniqueness: true, allow_blank: true, format: {with: /\.(gif|jpg|png)\Z/i, message: "Must be a valid image (GIF, JPG or PNG)"}
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(
+      title: "My Book",
+      description: "This is a really good book.",
+      image_url: "zzz.jpg"
+    )
+    
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  test "product img url must be valid" do
+    ok = ["img1.jpg", "img2.JpG"]
+    bad = ["hmm.doc", "trala.rb", "test.jpg\nkasper"]
+    
+    ok.each do |img|
+      product = Product.new(
+        title: "Test book",
+        description: "This is a book used for testing.",
+        price: 1,
+        image_url: img
+      )
+      
+      assert product.valid?, "#{img} should be valid."
+    end
+    
+    bad.each do |img|
+      product = Product.new(
+        title: "Test book",
+        description: "This is a book used for testing.",
+        price: 1,
+        image_url: img
+      )
+      
+      assert product.invalid?, "#{img} should be invalid."
+    end
+  end
 end
