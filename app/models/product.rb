22b43809364155143f7be7b49b6a5e6000191a52
Kasper Johansen <k@spernj.org>
2013-02-17 10:44:39 +0100

    Added title-length validation and validate-callback test. Also played with fixtures.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,14 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   
   validates :title, :description, :image_url, presence: true, uniqueness: true
+  validates :title, length: {in: 5..50}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url, uniqueness: true, allow_blank: true, format: {with: /\.(gif|jpg|png)\Z/i, message: "Must be a valid image (GIF, JPG or PNG)"}
+  
+  #Test validate callback.
+  before_validation :validate
+  
+  def validate
+    errors.add(:title, "Don't sell the author of this program!") if title.to_s.match /^\s*kasper\s+johansen\s*$/i
+  end
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -57,4 +57,50 @@ class ProductTest < ActiveSupport::TestCase
       assert product.invalid?, "#{img} should be invalid."
     end
   end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "Test unique title product.",
+      price: 1,
+      image_url: "test.jpg"
+    )
+    
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+  
+  test "product title must be a minimum of 5 characters and a maximum of 50" do
+    product = Product.new(
+      title: "abc",
+      description: "Test minimum title length.",
+      price: 1,
+      image_url: "test.jpg"
+    )
+    
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 5 characters)"], product.errors[:title]
+    
+    product = Product.new(
+      title: "a" * 51,
+      description: "Test maximum title length",
+      price: 1,
+      image_url: "test.jpg"
+    )
+    
+    assert product.invalid?
+    assert_equal ["is too long (maximum is 50 characters)"], product.errors[:title]
+  end
+  
+  test "it should not be possible to sell the author of this app (test validate-callback to method)" do
+    product = Product.new(
+      title: "Kasper Johansen",
+      description: "Test sell author",
+      price: 1,
+      image_url: "test.jpg"
+    )
+    
+    assert product.invalid?
+    assert_equal ["Don't sell the author of this program!"], product.errors[:title]
+  end
 end
